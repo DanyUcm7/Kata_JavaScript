515c8e66db8a08b23d1a809a64a46ee9b325dbd0
DanyUcm7 <dumgcr7mss@gmail.com>
2022-09-22 21:03:42 -0500

    Intro a JavaScript

---
+++ 0_IntroJS/main.js
@@ -0,0 +1,28 @@
+//JavaScript es un mensaje que se lee de arriba a abajo
+
+//De alert hasta document.write son SALIDAS los llamamos asi porque mostramos informacion
+
+// Como poner una alerta desde javascript las comente para que no den lata :)
+/*alert("Hola, mundo")
+alert("otro alert")*/
+
+//Enviar mensajes a la consola
+console.log("Este mensaje se autodestruira en 5 4 3...")
+console.log("otro mensaje")
+
+//Mostrar un error en la consola
+console.error("ERROR: No se encontro el archivo")
+
+//Mostrar un warning en la consola
+//El warning no es un error pero no es algo tan bueno
+console.warn("Estas añadiendo muchas alertas") 
+
+document.write("Texto escrito desde JavaScript")
+
+//Entradas solicita informacion
+
+//Comento los prompts para que no den lata 
+/*prompt("Introduce tu nombre")
+prompt("Introduce tu apellido")*/
+
+
