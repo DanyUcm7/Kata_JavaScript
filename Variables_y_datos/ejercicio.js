cd5e41c8d88c47b02ac414d5adc5f190447fe194
DanyUcm7 <dumgcr7mss@gmail.com>
2022-09-23 15:56:23 -0500

    Ejercicio resuelto

--- Variables_y_datos/ejercicio.js
+++ Variables_y_datos/ejercicio.js
@@ -6,22 +6,30 @@
 // 2. Rufo
 // 3. Chancla
 
+let mascota1 = "Pedrito"
+let mascota2 = "Rufo"
+let mascota3 = "Chancla"
+let frase = "Las quiero mucho"
 
 
 // Muestra en consola, accediendo a las variables, un mensaje como el siguiente:
 // "Mis mascotas son Pedrito, Rufo y Chancla. Las quiero mucho"
 
+console.log("Mis mascotas son " + mascota1 + ", " + mascota2 + " y " + mascota3 + ". " + frase)
 
 
 // Input y Output
 
 // Pedir mediante prompts el nombre de dos mascotas y mostrar en consola un mensaje como el siguiente:
 
+//let masc1 = prompt("Escribe el nombre de tu primera mascota")
+//let masc2 = prompt("Escribe el nombre de tu segunda mascota")
+let frase2 = "Quiero mucho a mis mascotas"
 
 
 // "Quiero mucho a mis mascotas mascota1 y mascota2"
 
-
+//console.log(frase2 + " " + masc1 + " y " + masc2)
 
 
 // NOTA: mascota1 y mascota2 corresponden a los nombres introducidos mediante prompts
@@ -30,15 +38,20 @@
 
 // Guarda la cadena de texto "100" en una variable
 
+let numero = "100"
 
 // Muestra el contenido de esta variable en consola
 
+console.log(numero)
+
 // Muestra el TIPO DE DATO de esta variable en consola
+
+console.log(typeof numero)
 
 // Transforma la variable de string a number
 
+let numeroTransformacion = parseInt(numero)
 
 // Muestra nuevamente el contenido de esta variable en consola
 
-
-// ¿Qué diferencias notas al mostrar en consola la variable antes y después de transformarla? ¿Qué es entonces un tipo de dato?
+console.log(numeroTransformacion)
